6f32ae055d7540da37d0edca37215dd601ed6f38
AterCattus <cat@ater.me>
2017-11-26 05:45:27 +0300

    add menu scene

---
+++ Assets/Scripts/FightBtn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FightBtn : MonoBehaviour {
+	private void Start()
+	{
+		var btn = gameObject.GetComponent<Button>();
+		btn.onClick.AddListener(() =>
+		{
+			SceneManager.LoadScene("Battle");
+		});
+	}
+}
